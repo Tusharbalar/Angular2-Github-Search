e0e65f941830618a7bc95a30452c73e90b9b309f
TusharBalar <balar.tushar1@gmail.com>
2016-08-09 22:43:50 +0530

    Create first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { GithubComponent } from './components/github.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><github></github>',
+    directives: [GithubComponent]
 })
 export class AppComponent { }
---
+++ app/components/github.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'github',
+    template: '<h1>GITHUB</h1>'
+})
+export class GithubComponent { }
