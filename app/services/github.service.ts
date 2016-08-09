92c6df5e6d06c613992eb35d0007729849a59bb6
TusharBalar <balar.tushar1@gmail.com>
2016-08-10 01:02:22 +0530

    Add clientID and secretId

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -6,18 +6,20 @@ import 'rxjs/add/operator/map';
 export class GithubService {
   
   private username = "Tusharbalar";
+  private client_id = "35ce64cf5a3d9c08f7eb";
+  private client_secret = "b79cbce4aae66a562dde9abf888ad5bad1d91c14";
 
   constructor(private _http : Http) {
     console.log("Github service started...")
   }
 
   getUser() {
-    return this._http.get("https://api.github.com/users/" + this.username)
+    return this._http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
       .map(res => res.json());
   }
 
   getRepos() {
-    return this._http.get("https://api.github.com/users/" + this.username + "/repos")
+    return this._http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
       .map(res => res.json());
   }
 
